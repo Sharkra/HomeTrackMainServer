a31029beda5a3e855d08b5ea1fabf2fd00c210d4
Tiago Ferreira <tmmferr@gmail.com>
2025-10-25 10:53:50 +0100

    Start env setup script

--- SatelliteServer/setup/env_setup.bash
+++ SatelliteServer/setup/env_setup.bash
@@ -1,5 +1,13 @@
 #!/bin/bash
 
 
+source helper_functions.sh
 
-echo "HELLO"
+# Start with a fresh Ubuntu install
+
+echo_blue "Starting environment setup"
+
+echo_blue "Installing Utility packages"
+installPackages openssh-server
+echo_green "Utility packages installed"
+
---
+++ SatelliteServer/setup/helper_functions.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+function echo_green() { echo -e "\033[1;32m$1\033[0m"; }
+function echo_red() { echo -e "\033[1;31m$1\033[0m"; }
+function echo_blue() { echo -e "\033[1;34m$1\033[0m"; }
+
+function checkPackagesInstalled() {
+  if dpkg -s "$@" > /dev/null 2>&1; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function checkRepoExists() {
+  if ls /etc/apt/sources.list.d | grep -q ".*${1}.*"; then
+    return 0
+  else
+    return 1
+  fi
+}
+
+function exportToBashrc() {
+    if grep -F -q -x "$@" $HOME/.bashrc; then
+        echo_green "Path already in bash. Not echoing it in."
+    else
+        echo_red "Path not in bash. Echo it into bashrc."
+        echo "$@" >> $HOME/.bashrc
+    fi
+    source $HOME/.bashrc
+}
+
+function installPackages() {
+  if checkPackagesInstalled "$@"; then
+    echo_green "Package(s) Installed"
+  else
+    echo_blue "Installing \"$@\""
+    sudo apt install -y "$@"
+  fi
+}
+
+function installDebPackage(){
+
+  if checkPackagesInstalled "$1"; then
+    echo_green "Package Installed"
+  else
+    echo_blue "Downloading $1"
+    cd /tmp
+    wget "$2" -O "$1".deb
+    echo_blue "Installing $1"
+    sudo dpkg -i "$1".deb
+    sudo apt install -f
+  fi
+}
