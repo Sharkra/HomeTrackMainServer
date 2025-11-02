ef89fca826d6204518d20db04ea31c32e6f5a610
Tiago Ferreira <tmmferr@gmail.com>
2025-11-02 11:40:25 +0000

    Testing Chart.JS

--- HomeTrackWebServer/application/pages.py
+++ HomeTrackWebServer/application/pages.py
@@ -4,7 +4,22 @@ bp = Blueprint("pages", __name__)
 
 @bp.route("/")
 def home():
-    return render_template("pages/home.html")
+    data=[
+        ("1", 21),
+        ("2", 10),
+        ("3", 50),
+        ("4", 35),
+        ("5", 29),
+        ("6", 1),
+        ("7", 74),
+        ("8", 42)
+    ]
+
+    labels = [row[0] for row in data]
+    values = [row[1] for row in data]
+
+    
+    return render_template("pages/home.html", labels=labels, values=values)
 
 @bp.route("/settings")
 def settings():
